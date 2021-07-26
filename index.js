32038cf07940badbea74b1a4a14fea78b1c02f42
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-07-27 01:49:23 +0530

    css: final look

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var enter=document.querySelector("#enter");
 var output=document.querySelector("#output");
 var outputgif=document.querySelector("#outputgif");
  const profitGIF =`<img align="centre" src="http://arbitrageprofitspy.com/wp-content/uploads/2016/01/make-it-rain2.gif"  " alt="funny GIF" width="24%"  />`
- const lossGIF=`<img align="centre" src="https://media1.tenor.com/images/49cb833f033c5de3f98107b70d36fea6/tenor.gif?itemid=10128252" width="24%"/>`
+ const lossGIF=`<img align="centre" src="https://media1.tenor.com/images/49cb833f033c5de3f98107b70d36fea6/tenor.gif?itemid=10128252" width="30%"/>`
  const moreloss=`<img src="https://i.pinimg.com/originals/d3/5c/6d/d35c6d22a274ce7b4652a1c6e17cca24.gif"/>`
 
 
@@ -31,11 +31,11 @@ function profitloss(event){
                     document.body.style.backgroundRepeat = "no-repeat";
                      document.body.style.backgroundSize = "cover";
                    
-                      output.innerHTML="   lost more than 50% 😭 <br> LOSS of "+ percentloss+"%"+" Your total loss is of rs. "+loss;
+                      output.innerHTML=" Lost more than 50% 😭 <br> LOSS of "+ percentloss+" %"+"  Your total loss is of rs. "+loss;
                 }
                 else
                 {
-                output.innerHTML="LOSS of"+percentloss+"%"+" your total loss is of rs."+loss;
+                output.innerHTML="LOSS of "+percentloss+" %"+" Your total loss is of rs."+loss +"😶";
                 outputgif.innerHTML=lossGIF;
                 
                 }
@@ -45,8 +45,8 @@ function profitloss(event){
             {
                 const profit = (pricecurrently - purchasePrice) * quantityNo;
             const percentProfit = (((pricecurrently- purchasePrice)*100)/purchasePrice).toFixed(2);
-            output.innerHTML ="GAIN OF " + percentProfit +
-            " %. Your total profit is of  Rs." + profit;
+            output.innerHTML =" GAIN of " + percentProfit +
+            " %. Your total profit is of  Rs. " + profit +"😍";
             outputgif.style.display="block";
             outputgif.innerHTML=profitGIF;
             
